f70a76536392b3705e851424d056185001c155df
ItsThankK <tkingsway3@gmail.com>
2025-08-25 14:31:21 +0100

    pause

--- guessing-game-starter/script.js
+++ guessing-game-starter/script.js
@@ -8,4 +8,7 @@ document.querySelector('.check').addEventListener('click', function () {
   console.log(document.querySelector('.guess').value);
 });
 
-document.querySelector("body").style.backgroundColor = "red";
+document.querySelector('body').style.backgroundColor = 'red';
+
+// Modal window
+document.querySelector('.modal-section').style.backgroundColor = "red";
