e59a34462ae132c96c11d800f3e8c30208ff84a2
ItsThankK <tkingsway3@gmail.com>
2025-08-25 10:04:24 +0100

    finish functions

--- part-2-starter/script.js
+++ part-2-starter/script.js
@@ -1,7 +1,38 @@
-"use strict"
+"use strict";
 
 function logger() {
   console.log("I am a logger function");
 }
 
 logger();
+
+// declaration
+function calAge(birthYear) {
+  return 2025 - birthYear;
+}
+console.log(calAge(15));
+
+// expression
+const calAge2 = function (birthYear) {
+  return 3000 - birthYear;
+};
+console.log(calAge2(299));
+
+// arrow functions
+const calAge3 = (birthYear) => 2027 - birthYear;
+const calAge4 = (birthYear, birthYear2) => {
+  console.log(birthYear, birthYear2);
+};
+
+// functions calling other functions
+function callMe(myName) {
+  return `My name is ${myName}`;
+}
+
+function letMyName(myName) {
+  let result = callMe(myName);
+  console.log(result);
+  
+}
+
+letMyName("Elon Musk");
