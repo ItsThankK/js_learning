8169a61d0b3e998207938ce1cc314bf8e8b3fe50
ItsThankK <tkingsway3@gmail.com>
2025-08-25 19:12:46 +0100

    get the rolling dice to work

--- pig-game-starter/script.js
+++ pig-game-starter/script.js
@@ -12,3 +12,10 @@ const btnHold = document.querySelector(".btn--hold");
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add("hidden");
+
+//Rolling dice functionality
+btnNRoll.addEventListener("click", function() {
+  diceEl.classList.remove("hidden");
+  let dice = Math.floor(Math.random() * 6) + 1;
+  diceEl.src = `dice-${dice}.png`;
+});
