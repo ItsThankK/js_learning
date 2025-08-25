17b2abc0d05a5ff24a52a8f62f8ce0d457dcf69d
ItsThankK <tkingsway3@gmail.com>
2025-08-25 13:17:18 +0100

    manipulate on button click

--- guessing-game-starter/script.js
+++ guessing-game-starter/script.js
@@ -1,4 +1,9 @@
 'use strict';
 
-let domManupilation = document.querySelector('.message').textContent = "DOM Manipulated!";
-console.log(domManupilation);
+// let domManupilation = (document.querySelector('.message').textContent =
+//   'DOM Manipulated!');
+// console.log(domManupilation);
+
+document.querySelector('.check').addEventListener('click', function() {
+  console.log(document.querySelector('.guess').value);
+});
