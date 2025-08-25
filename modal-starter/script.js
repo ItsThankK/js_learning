4c799ae50b8b9178a4d9393d19f3da22f767f3c8
ItsThankK <tkingsway3@gmail.com>
2025-08-25 15:11:26 +0100

    open and close modal

--- modal-starter/script.js
+++ modal-starter/script.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnsOpenModal = document.querySelectorAll(".show-modal");
+console.log(btnsOpenModal);
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  const element = btnsOpenModal[i];
+  element.addEventListener(
+    "click", function() {
+    console.log("button clicked");
+    document.querySelector(".modal").classList.remove("hidden");
+    document.querySelector(".overlay").classList.remove("hidden");
+  }
+);
+}
+
+btnCloseModal.addEventListener("click", function() {
+  document.querySelector('.modal').classList.add("hidden");
+  document.querySelector(".overlay").classList.add("hidden");
+});
