8836668687a21a1abac5153f122f870b7401d4cb
ItsThankK <tkingsway3@gmail.com>
2025-08-25 15:55:32 +0100

    open and close modal - refactor

--- modal-starter/script.js
+++ modal-starter/script.js
@@ -1,23 +1,26 @@
 'use strict';
 
-const modal = document.querySelector(".modal");
-const overlay = document.querySelector(".overlay");
-const btnCloseModal = document.querySelector(".close-modal");
-const btnsOpenModal = document.querySelectorAll(".show-modal");
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
 console.log(btnsOpenModal);
+
+const openModalFunction = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModalFunction = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
 
 for (let i = 0; i < btnsOpenModal.length; i++) {
   const element = btnsOpenModal[i];
-  element.addEventListener(
-    "click", function() {
-    console.log("button clicked");
-    document.querySelector(".modal").classList.remove("hidden");
-    document.querySelector(".overlay").classList.remove("hidden");
-  }
-);
+  element.addEventListener('click', openModalFunction);
 }
 
-btnCloseModal.addEventListener("click", function() {
-  document.querySelector('.modal').classList.add("hidden");
-  document.querySelector(".overlay").classList.add("hidden");
-});
+btnCloseModal.addEventListener('click', closeModalFunction);
+overlay.addEventListener("click", closeModalFunction)
+
