b93780d6a637d45aebee661bbbe532c68fb3f566
ItsThankK <tkingsway3@gmail.com>
2025-08-26 14:36:12 +0100

    complete pig game project

--- pig-game-starter/script.js
+++ pig-game-starter/script.js
@@ -14,16 +14,28 @@ const btnNew = document.querySelector('.btn--new');
 const btnNRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
-// Starting conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
-restart.classList.add('hidden');
+let scores, currentScore, activePlayer, playing;
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+const init = function () {
+  // Starting conditions
+  diceEl.classList.add('hidden');
+  restart.classList.add('hidden');
+
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+};
+init();
 
 function switchPlayer() {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -67,7 +79,7 @@ btnHold.addEventListener('click', function () {
     currentScore = 0;
     document.getElementById(`current--${activePlayer}`).textContent = 0;
 
-    if (scores[activePlayer] >= 10) {
+    if (scores[activePlayer] >= 100) {
       //End the game session
       document
         .querySelector(`.player--${activePlayer}`)
@@ -76,10 +88,15 @@ btnHold.addEventListener('click', function () {
         .querySelector(`.player--${activePlayer}`)
         .classList.remove('player--active');
       playing = false;
+      diceEl.classList.add('hidden');
     } else {
       switchPlayer();
     }
   } else {
     restartGameAnimation();
   }
+});
+
+btnNew.addEventListener('click', function () {
+  init();
 });
