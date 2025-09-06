ab14de315484ebab29ff957940979986dc14250b
ItsThankK <tkingsway3@gmail.com>
2025-09-06 03:37:49 +0100

    start learning about firstclass and higher order functionsn

--- functions-starter/script.js
+++ functions-starter/script.js
@@ -1,1 +1,58 @@
 'use strict';
+
+const bookings = [];
+const createBooking = function (
+  flightNum,
+  numPassengers = 1,
+  price = 199 * numPassengers
+) {
+  //ES6 Way price = 199
+
+  // ES5 Way
+  // numPassengers ||= 1;
+  // price ||= 199;
+
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+createBooking(`LH123`);
+/*
+createBooking(`LH123`, 2, 800);
+createBooking(`LH123`, 2,);
+createBooking(`LH123`, undefined ,2);
+*/
+const flight = `LH234`;
+const jonas = {
+  name: `Jonas Schmedtmann`,
+  passport: 48949403994,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = `LH999`;
+  passenger.name = `Mr. ` + passenger.name;
+
+  if (passenger.passport === 48949403994) {
+    alert(`Check in`);
+  } else {
+    alert(`Wrong passport!`);
+  }
+};
+// checkIn(flight, jonas);
+console.log(flight, jonas);
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 100000000000);
+};
+
+newPassport(jonas);
+checkIn(flight, jonas);
+
+// Passing by reference OR values?
+// Javascript DOESN'T have passing by reference/
+// Javascript pass by value
+
