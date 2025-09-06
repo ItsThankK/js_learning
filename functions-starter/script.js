82c575fb091be38cb4d1e59778ac24404a9fb0a0
ItsThankK <tkingsway3@gmail.com>
2025-09-06 04:22:36 +0100

    start learning about call and apply methods on fuctions

--- functions-starter/script.js
+++ functions-starter/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/*
 const bookings = [];
 const createBooking = function (
   flightNum,
@@ -21,11 +22,11 @@ const createBooking = function (
   bookings.push(booking);
 };
 createBooking(`LH123`);
-/*
 createBooking(`LH123`, 2, 800);
 createBooking(`LH123`, 2,);
 createBooking(`LH123`, undefined ,2);
 */
+/*
 const flight = `LH234`;
 const jonas = {
   name: `Jonas Schmedtmann`,
@@ -49,10 +50,50 @@ const newPassport = function (person) {
   person.passport = Math.trunc(Math.random() * 100000000000);
 };
 
-newPassport(jonas);
-checkIn(flight, jonas);
+// newPassport(jonas);
+// checkIn(flight, jonas);
+*/
 
 // Passing by reference OR values?
 // Javascript DOESN'T have passing by reference/
-// Javascript pass by value
+// Javascript pass a value
 
+const oneWord = function (str) {
+  return str.replace(/ /g, ``).toLowerCase();
+};
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(` `);
+  return [first.toUpperCase(), ...others].join(` `);
+};
+
+// Higher order function/
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string; ${fn(str)}`);
+  console.log(`Transformed by: ${fn.name}`);
+};
+transformer(`JavaScript is the best!`, upperFirstWord);
+console.log(` `);
+transformer(`JavaScript is the best!`, oneWord);
+
+// JavaScript callbacks!/
+const high5 = function () {
+  console.log(`👋`);
+};
+document.body.addEventListener(`click`, high5);
+
+[`Jonas`, `Martha`, `Adam`].forEach(high5);
+
+// Fuctions returning functions/
+const greet = function (greeting) {
+  return function (name) {
+    console.log(`${greeting} ${name}`);
+  };
+};
+const greeterHey = greet(`Hey`);
+greeterHey(`ThankK`);
+greeterHey(`Jonas`);
+greet(`Hello`)(`World`);
+
+// Challenge
+const greetArr = greeting => name => console.log(`${greeting} ${name}`);
