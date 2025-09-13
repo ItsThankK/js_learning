c8dc435bfad0fda8766e86f0ea849b8cc01f1c21
ItsThankK <tkingsway3@gmail.com>
2025-09-13 20:44:26 +0100

    finishing learing to select, create, and delete elements

--- advanced-dom-starter/script.js
+++ advanced-dom-starter/script.js
@@ -8,7 +8,8 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
-const openModal = function () {
+const openModal = function (e) {
+  e.preventDefault();
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -18,8 +19,7 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+btnsOpenModal.forEach(btn => btn.addEventListener(`click`, openModal));
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
@@ -29,3 +29,45 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+////////////////////////////////////////////////////
+// selecting elements
+/*
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+
+document.querySelector(`header`);
+const allSection = document.querySelectorAll(`.section`);
+console.log(allSection);
+
+document.getElementById(`section--1`);
+const allButtons = document.getElementsByTagName(`button`);
+console.log(allButtons);
+
+console.log(document.getElementsByClassName(`btn`));
+*/
+
+// creating and inserting elements
+
+// .insertAdjacentHTML
+const message = document.createElement(`div`);
+message.classList.add(`cookie-message`);
+// message.textContent = `We use cookie for improved functionality and analytics.`;
+message.innerHTML = `We use cookie for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>`;
+
+const header = document.querySelector(`header`);
+// header.prepend(message); // before as a child
+// header.append(message); // after as a child
+// header.append(message.cloneNode(true)) // have it at different places
+
+// header.before(message); //truely before the header
+header.after(message);
+
+// deleting elements
+document
+  .querySelector(`.btn--close-cookie`)
+  .addEventListener(`click`, function () {
+    // message.remove() //new method
+    message.parentElement.removeChild(message); // old method
+  });
