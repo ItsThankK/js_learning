f0bc8ccf06d2748fa6a4f977a3724df295303505
ItsThankK <tkingsway3@gmail.com>
2025-09-14 16:48:19 +0100

    finish dom trasversing

--- advanced-dom-starter/script.js
+++ advanced-dom-starter/script.js
@@ -88,7 +88,6 @@ document.querySelector(`.nav__links`).addEventListener(`click`, function (e) {
     const id = e.target.getAttribute(`href`);
     document.querySelector(`${id}`).scrollIntoView({ behavior: `smooth` });
   }
-  //
 });
 
 ////////////////////////////////////////////////////
@@ -130,107 +129,138 @@ document
 .addEventListener(`click`, function () {
   // message.remove() //new method
   message.parentElement.removeChild(message); // old method
-});
-
-// styles
-message.style.backgroundColor = `#37383d`;
-message.style.width = `120%`;
-
-// console.log(message.style.backgroundColor);
-// console.log(getComputedStyle(message).color);
-
-message.style.height =
-Number.parseFloat(getComputedStyle(message).height, 10) + 30 + `px`;
-
-// console.log(getComputedStyle(message).height);
-
-document.documentElement.style.setProperty(`--color-primary`, `orangered`);
-
-//  attributes classes
-const logo = document.querySelector(`.nav__logo`);
-console.log(logo.alt);
-console.log(logo.className);
-
-console.log(logo.designer); // undefined - not a standard artribute
-logo.setAttribute(`designer`, `Bankist`);
-console.log(logo.getAttribute`designer`);
-
-logo.alt = `Beautiful minimalist logo`;
-
-console.log(logo.src);
-console.log(logo.getAttribute(`src`));
-
-const link = document.querySelector(`.nav__link--btn`);
-console.log(link.href);
-console.log(link.getAttribute(`href`));
-
-// data attributes
-logo.setAttribute(`data-version-number`, `3.0`);
-console.log(logo.dataset.versionNumber);
-
-// classes
-logo.classList.add(`c`);
-logo.classList.remove(`c`);
-logo.classList.toggle(`c`);
-logo.classList.contains(`c`);
-
-// logo.className = `jonas`; // do not use!
-*/
+  });
+  
+  // styles
+  message.style.backgroundColor = `#37383d`;
+  message.style.width = `120%`;
+  
+  // console.log(message.style.backgroundColor);
+  // console.log(getComputedStyle(message).color);
+  
+  message.style.height =
+  Number.parseFloat(getComputedStyle(message).height, 10) + 30 + `px`;
+  
+  // console.log(getComputedStyle(message).height);
+  
+  document.documentElement.style.setProperty(`--color-primary`, `orangered`);
+  
+  //  attributes classes
+  const logo = document.querySelector(`.nav__logo`);
+  console.log(logo.alt);
+  console.log(logo.className);
+  
+  console.log(logo.designer); // undefined - not a standard artribute
+  logo.setAttribute(`designer`, `Bankist`);
+  console.log(logo.getAttribute`designer`);
+  
+  logo.alt = `Beautiful minimalist logo`;
+  
+  console.log(logo.src);
+  console.log(logo.getAttribute(`src`));
+  
+  const link = document.querySelector(`.nav__link--btn`);
+  console.log(link.href);
+  console.log(link.getAttribute(`href`));
+  
+  // data attributes
+  logo.setAttribute(`data-version-number`, `3.0`);
+  console.log(logo.dataset.versionNumber);
+  
+  // classes
+  logo.classList.add(`c`);
+  logo.classList.remove(`c`);
+  logo.classList.toggle(`c`);
+  logo.classList.contains(`c`);
+  
+  // logo.className = `jonas`; // do not use!
+  */
 
 // Events
 /*
-const h1 = document.querySelector(`h1`);
-
-// old method
-// h1.onmouseenter = e =>
-//   alert(`addEventlistener: Great! You are reading the heading`);
-
-// new method
-// h1.addEventListener(`mouseenter`, function (e) {
-//   alert(`addEventlistener: Great! You are reading the heading`);
-// });
-
-const alertH1 = function (e) {
-  alert(`addEventlistener: Great! You are reading the heading`);
-
-  h1.removeEventListener(`mouseenter`, alertH1);
-};
-
-h1.addEventListener(`mouseenter`, alertH1);
-
-setTimeout(() => h1.removeEventListener(`mouseenter`, alertH1), 10000);
-*/
+ const h1 = document.querySelector(`h1`);
+ 
+ // old method
+ // h1.onmouseenter = e =>
+  //   alert(`addEventlistener: Great! You are reading the heading`);
+ 
+ // new method
+ // h1.addEventListener(`mouseenter`, function (e) {
+  //   alert(`addEventlistener: Great! You are reading the heading`);
+  // });
+  
+  const alertH1 = function (e) {
+    alert(`addEventlistener: Great! You are reading the heading`);
+    
+    h1.removeEventListener(`mouseenter`, alertH1);
+    };
+    
+    h1.addEventListener(`mouseenter`, alertH1);
+    
+    setTimeout(() => h1.removeEventListener(`mouseenter`, alertH1), 10000);
+    */
 
 // Bubbling
 // event propagation: bubbling and capturing
 /*
-const randomInt = (min, max) =>
-  Math.floor(Math.random() * (max - min + 1) + min);
-
-const randomColor = () =>
-  `rgba(${randomInt(0, 255)}, ${randomInt(0, 255)}, ${randomInt(0, 255)})`;
-
-console.log(randomColor());
-
-document.querySelector(`.nav__link`).addEventListener(`click`, function (e) {
-  this.style.backgroundColor = randomColor();
-  console.log(`link`, e.target, e.currentTarget);
-
-  // stop propagation
-  // e.stopPropagation();
-});
-
-document.querySelector(`.nav__links`).addEventListener(`click`, function (e) {
-  this.style.backgroundColor = randomColor();
-  console.log(`container`, e.target, e.currentTarget);
-});
-
-document.querySelector(`.nav`).addEventListener(
-  `click`,
-  function (e) {
+   const randomInt = (min, max) =>
+    Math.floor(Math.random() * (max - min + 1) + min);
+   
+   const randomColor = () =>
+    `rgba(${randomInt(0, 255)}, ${randomInt(0, 255)}, ${randomInt(0, 255)})`;
+   
+   console.log(randomColor());
+   
+   document.querySelector(`.nav__link`).addEventListener(`click`, function (e) {
     this.style.backgroundColor = randomColor();
-    console.log(`nav`, e.target, e.currentTarget);
-  },
-  true
-);
-*/
+    console.log(`link`, e.target, e.currentTarget);
+    
+    // stop propagation
+    // e.stopPropagation();
+    });
+    
+    document.querySelector(`.nav__links`).addEventListener(`click`, function (e) {
+      this.style.backgroundColor = randomColor();
+      console.log(`container`, e.target, e.currentTarget);
+      });
+      
+      document.querySelector(`.nav`).addEventListener(
+        `click`,
+        function (e) {
+          this.style.backgroundColor = randomColor();
+          console.log(`nav`, e.target, e.currentTarget);
+          },
+          true
+          );
+          */
+
+// dom trasversing - walking through the dom
+const h1 = document.querySelector(`h1`);
+
+// going downwards: child
+console.log(h1.querySelectorAll(`.highlight`));
+console.log(h1.childNodes); // not used - nodeList
+console.log(h1.children); // only for direct children - html collection
+h1.firstElementChild.style.color = `white`;
+h1.lastElementChild.style.color = `orangered`;
+
+// going upwards: parents
+console.log(h1.parentNode);
+console.log(h1.parentElement);
+
+h1.closest(`.header`).style.background = `var(--gradient-secondary)`; // finds parents
+
+h1.closest(`h1`).style.background = `var(--gradient-primary)`; // finds parents
+
+// going sideways: siblings
+// we can only access direct siblings
+console.log(h1.previousElementSibling);
+console.log(h1.nextElementSibling);
+console.log(h1.parentElement.children);
+
+[...h1.parentElement.children].forEach(el => {
+  if (el !== h1) {
+    el.style.transform = `scale(0.5)`
+  }
+})
+
