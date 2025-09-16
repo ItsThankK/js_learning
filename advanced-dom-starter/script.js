0160de46d1d5f65d2229020b63eded4845d8a5ff
ItsThankK <tkingsway3@gmail.com>
2025-09-16 19:24:48 +0100

    finish revealing sections Intersection observer api

--- advanced-dom-starter/script.js
+++ advanced-dom-starter/script.js
@@ -178,6 +178,29 @@ const observerHeading = new IntersectionObserver(
   }
 );
 observerHeading.observe(header);
+// ////////////////////////////////
+// reveal sections
+const allSections = document.querySelectorAll(`section`);
+
+const revealSection = function (entries, observer) {
+  const [entry] = [...entries];
+  console.log(entry);
+
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove(`section--hidden`);
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSections.forEach(section => {
+  section.classList.add(`section--hidden`);
+  sectionObserver.observe(section);
+});
+
 ////////////////////////////////////////////////////
 // selecting elements
 /*
