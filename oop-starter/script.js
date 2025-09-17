614365b288b3851c0ea0cf330c49643275ee33a5
ItsThankK <tkingsway3@gmail.com>
2025-09-17 13:17:16 +0100

    constructor functions and the new operator

--- oop-starter/script.js
+++ oop-starter/script.js
@@ -1,1 +1,27 @@
 'use strict';
+
+// constructor fuctions & the new operator
+const Person = function (firstName, birthYear) {
+  // Instance properties
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+  // Bad practice! Never create a method inside of a constructor
+  // this.calcAge = function () {
+  //   console.log(2037 - this.birthYear);
+  // }
+};
+// 4 steps to create this //
+// 1. New {} is created
+// 2. function is called, this = {}
+// 3. {} linked to a prototype
+// 4. fuction automatically returns the {}
+const jonas = new Person(`Jonas`, 1991);
+console.log(jonas);
+
+const maltilda = new Person(`Maltilda`, 2017);
+const jack = new Person(`Jack`, 1975);
+console.log(maltilda, jack);
+
+const j = 1;
+console.log(jonas instanceof Person); //true
+console.log(j instanceof Person); //false
