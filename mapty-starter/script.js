1fccc1241715b6da5b0e94b5398999e411a0211b
ItsThankK <tkingsway3@gmail.com>
2025-09-19 10:35:29 +0100

    displaying the map marker

--- mapty-starter/script.js
+++ mapty-starter/script.js
@@ -21,17 +21,34 @@ if (navigator.geolocation) {
       const coords = [latitude, longitude];
 
       // leaflet API
-      const map = L.map('map').setView(coords, 13);
+      const map = L.map('map').setView(coords, 12);
 
       L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
-      L.marker(coords)
-        .addTo(map)
-        .bindPopup('A pretty CSS popup.<br> Easily customizable.')
-        .openPopup();
+      // console.log(map);
+      // map event listener
+      map.on(`click`, function (mapEvent) {
+        console.log(mapEvent);
+        const { lat, lng } = mapEvent.latlng;
+        console.log(lat, lng);
+
+        L.marker([lat, lng])
+          .addTo(map)
+          .bindPopup(
+            L.popup({
+              maxWidth: 250,
+              minWidth: 100,
+              autoClose: false,
+              closeOnClick: false,
+              className: `running-popup`,
+            })
+          )
+          .setPopupContent(`Workout`)
+          .openPopup();
+      });
     },
     function () {
       alert(`Could not get your location / position`);
