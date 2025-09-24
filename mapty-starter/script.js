cafbd966031d51ee3ed1dcc1e788fa394022fb9c
ItsThankK <tkingsway3@gmail.com>
2025-09-24 11:00:49 +0100

    move to marker on click

--- mapty-starter/script.js
+++ mapty-starter/script.js
@@ -5,6 +5,7 @@
 class Workout {
   date = new Date();
   id = (Date.now() + ``).slice(-10);
+  clicks = 0;
 
   constructor(coords, distance, duration) {
     this.coords = coords; //[lat, lng]
@@ -19,6 +20,10 @@ class Workout {
     this.description = `${this.type[0].toUpperCase() + this.type.slice(1)} on ${
       months[this.date.getMonth()]
     } ${this.date.getDay()}`;
+  }
+
+  click() {
+    this.clicks++;
   }
 }
 
@@ -68,15 +73,15 @@ const inputElevation = document.querySelector('.form__input--elevation');
 
 class App {
   #map;
+  #mapZoomLevel = 13;
   #mapEvent;
   #workouts = [];
 
   constructor() {
     this._getPosition();
-
     form.addEventListener(`submit`, this._newWorkout.bind(this));
-
     inputType.addEventListener(`change`, this._toggleElevationField);
+    containerWorkouts.addEventListener(`click`, this._moveToPopup.bind(this));
   }
 
   _getPosition() {
@@ -97,7 +102,7 @@ class App {
     const coords = [latitude, longitude];
 
     // leaflet API
-    this.#map = L.map('map').setView(coords, 12);
+    this.#map = L.map('map').setView(coords, this.#mapZoomLevel);
 
     L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
       attribution:
@@ -252,6 +257,25 @@ class App {
     }
 
     form.insertAdjacentHTML(`afterend`, html);
+  }
+
+  _moveToPopup(e) {
+    const workoutEl = e.target.closest(`.workout`);
+    if (!workoutEl) return;
+
+    const workout = this.#workouts.find(
+      workout => workout.id === workoutEl.dataset.id
+    );
+
+    this.#map.setView(workout.coords, this.#mapZoomLevel, {
+      animate: true,
+      pan: {
+        duration: 1,
+      },
+    });
+
+    // Using the public interface
+    workout.click();
   }
 }
 
