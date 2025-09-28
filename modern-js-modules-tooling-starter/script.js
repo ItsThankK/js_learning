737195f0fcde079918068b3719eb4e33dc19d841
ItsThankK <tkingsway3@gmail.com>
2025-09-28 08:40:30 +0100

    top level await (es2022)

--- modern-js-modules-tooling-starter/script.js
+++ modern-js-modules-tooling-starter/script.js
@@ -4,7 +4,7 @@
 // addToCart(`bread`, 5);
 // console.log(price, tq);
 
-// console.log(`Importing module`);
+console.log(`Importing module`);
 // import * as ShoppingCart from './shoppingCart.js'
 // ShoppingCart.addToCart(`bread`, 5)
 
@@ -14,6 +14,22 @@ add(`bread`, 2);
 add(`bread`, 5);
 add(`bread`, 4);
 
-import {cart} from './shoppingCart.js'
+import { cart } from './shoppingCart.js';
 console.log(cart);
 
+// top level await
+// console.log(`Start fetching`);
+// const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+// const data = await res.json();
+// console.log(data);
+// console.log(`Something`);
+
+const getLastPost = async function () {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+  const data = await res.json();
+
+  return { title: data.at(-1).title, text: data.at(-1).body };
+};
+
+const lastPost = await getLastPost();
+console.log(lastPost);
