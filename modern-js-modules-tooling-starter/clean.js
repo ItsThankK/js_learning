74fa80107e269ee7ee0781c06216dede14364eed
ItsThankK <tkingsway3@gmail.com>
2025-09-29 07:09:56 +0100

    fix code - clean code - 2

--- modern-js-modules-tooling-starter/clean.js
+++ modern-js-modules-tooling-starter/clean.js
@@ -1,4 +1,6 @@
-const budget = [
+'use strict';
+
+const budget = Object.freeze([
   { value: 250, description: 'Sold old TV 📺', user: 'jonas' },
   { value: -45, description: 'Groceries 🥑', user: 'jonas' },
   { value: 3500, description: 'Monthly salary 👩‍💻', user: 'jonas' },
@@ -7,18 +9,25 @@ const budget = [
   { value: -20, description: 'Candy 🍭', user: 'matilda' },
   { value: -125, description: 'Toys 🚂', user: 'matilda' },
   { value: -1800, description: 'New Laptop 💻', user: 'jonas' },
-];
+]);
 
-const spendingLimits = {
+const spendingLimits = Object.freeze({
   jonas: 1500,
   matilda: 100,
-};
+});
 
 const getLimit = user => spendingLimits?.[user] ?? 0;
 
-const addExpense = function (value, description, user = `jonas`) {
+// pure function
+const addExpense = function (
+  state,
+  limits,
+  value,
+  description,
+  user = `jonas`
+) {
   // if (!user) user = 'jonas'; //fixed
-  user = user.toLowerCase();
+  const cleanUser = user.toLowerCase();
 
   // let lim;
   // if (spendingLimits[user]) {
@@ -31,14 +40,15 @@ const addExpense = function (value, description, user = `jonas`) {
 
   // const limit = getLimit(user);
 
-  if (value <= getLimit(user)) {
-    budget.push({ value: -value, description, user });
-  }
+  return value <= getLimit(cleanUser)
+    ? [...state, { value: -value, description, user: cleanUser }]
+    : state;
+  // budget.push({ value: -value, description, user: cleanUser });
 };
-addExpense(10, 'Pizza 🍕');
-addExpense(100, 'Going to movies 🍿', 'Matilda');
-addExpense(200, 'Stuff', 'Jay');
-console.log(budget);
+const newBudget1 = addExpense(budget, spendingLimits, 10, 'Pizza 🍕');
+addExpense(budget, spendingLimits, 100, 'Going to movies 🍿', 'Matilda');
+addExpense(budget, spendingLimits, 200, 'Stuff', 'Jay');
+console.log(newBudget1);
 
 const checkExpenses = function () {
   // let limit;
@@ -68,4 +78,3 @@ const logBigExpenses = function (bigLimit) {
 };
 
 logBigExpenses(1000);
-
